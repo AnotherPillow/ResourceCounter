d39e3f2e6c2b3b61cf6ac99ecbc8dbf2d6991f99
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2025-03-22 18:50:47 +0000

    gradle please

--- src/main/java/com/anotherpillow/resourcecounter/ResourceCounter.java
+++ src/main/java/com/anotherpillow/resourcecounter/ResourceCounter.java
@@ -1,16 +1,7 @@
 package com.anotherpillow.resourcecounter;
 
-import com.google.common.collect.Lists;
-import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.common.config.Property;
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-
-import java.io.File;
-import java.util.HashMap;
-import java.util.List;
 
 @Mod(
         modid = ResourceCounter.MODID,
